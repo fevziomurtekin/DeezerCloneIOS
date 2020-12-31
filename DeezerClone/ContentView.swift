8ae4f9c92606be8fdd43a447565be4041f1f3172
fevziomurtekin <fevziomurtekin@gmail.com>
2020-12-31 20:02:39 +0300

    Add basically splash view

--- DeezerClone/ContentView.swift
+++ DeezerClone/ContentView.swift
@@ -1,17 +1,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var isShownSplash:Bool = true
+    
+    var body: some View {
+        VStack {
+            if self.isShownSplash {
+                SplashView()
+            } else {
+                HomeView()
+            }
+        }.onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5){
+                withAnimation{
+                    self.isShownSplash = false
+                }
+            }
+        }
+    }
+}
+
+struct HomeView: View {
+    var body: some View {
+        Text("Item")
+    }
+}
+
+struct SplashView: View {
     var body: some View {
         Color.black
             .ignoresSafeArea()
             .overlay(
                 VStack(content: {
                     Text("DeezerClone")
-                        .font(.title)
+                        .font(Font.largeTitle)
                         .bold()
                         .foregroundColor(.white)
                 })
-            )
+        )
     }
 }
 
